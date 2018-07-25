13e3f76c0fba88c122af2fb6ee3c4270a66dc128
Bill Liu <liujidon@gmail.com>
2018-07-24 22:10:36 -0400

    css formatting for login and poll

--- public/slunch-web/src/app/poll-option/poll-option.component.ts
+++ public/slunch-web/src/app/poll-option/poll-option.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { PollOption } from '../poll-option';
+import { AuthService } from '../providers/auth.service';
 
 @Component({
   selector: 'app-poll-option',
@@ -8,10 +9,14 @@ import { PollOption } from '../poll-option';
 })
 export class PollOptionComponent implements OnInit {
   @Input() pollOption: PollOption;
-
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
+
+  myvote() {
+      return this.pollOption.votes.includes(this.authService.getUsername());
+  }
+
 
 }
