76b81f47d7c6c03b2e1de565ccde16c01801a3cf
michaelwang1997 <michaelw.wang@mail.utoronto.ca>
2018-10-09 01:11:42 -0400

    update functions so that making new poll resets vote status

--- functions/index.js
+++ functions/index.js
@@ -7,72 +7,120 @@ admin.initializeApp();
 
 // Listen for updates to any `transactions` document.
 exports.updateAccountBalance = functions.firestore
-  .document('transactions/{transactionId}')
-  .onUpdate((change, context) => {
-    const transaction = change.after.data();
-    const previousTransaction = change.before.data();
-    console.log("Transaction update: ", transaction);
+    .document('transactions/{transactionId}')
+    .onUpdate((change, context) => {
+        const transaction = change.after.data();
+        const previousTransaction = change.before.data();
+        console.log("Transaction update: ", transaction);
 
-    let status = transaction.status;
-    if (status === null || status !== "done" || transaction.accountid === null)
-      return null;
-    if (transaction.price === previousTransaction.price)
-      return null;
+        let status = transaction.status;
+        if (status === null || status !== "done" || transaction.accountid === null)
+            return null;
+        if (transaction.price === previousTransaction.price)
+            return null;
 
-    console.log("Updating account: ", transaction.accountid);
-    let account = admin.firestore().doc(transaction.accountid);
+        console.log("Updating account: ", transaction.accountid);
+        let account = admin.firestore().doc(transaction.accountid);
 
-    return account.get().then(doc => {
-      let spending = 0;
-      if(previousTransaction.status === transaction.status){
-        spending = transaction.price - previousTransaction.price;
-      }
-      else{
-        spending = transaction.price;
-      }
+        return account.get().then(doc => {
+            let spending = 0;
+            if (previousTransaction.status === transaction.status) {
+                spending = transaction.price - previousTransaction.price;
+            }
+            else {
+                spending = transaction.price;
+            }
 
-      let newBalance = doc.data().balance - spending;
-      console.log("New balance: ", newBalance);
-      return account.update({
-        balance: newBalance
-      });
+            let newBalance = doc.data().balance - spending;
+            console.log("New balance: ", newBalance);
+            return account.update({
+                balance: newBalance
+            });
+        });
     });
-  });
 
-// exports.updateVoteStatus = functions.firestore
-//     .document('polls/{pollId}')
+exports.updateVoteStatus = functions.firestore
+    .document('poll/{pollId}')
+    .onCreate((snap, context) => {
+        return admin.firestore().collection('accounts').get().then(querySnapshot => {
+            return querySnapshot.forEach(documentSnapshot => {
+                let account = admin.firestore().collection('accounts').doc(documentSnapshot.id);
+                if (documentSnapshot.data().voteStatus !== "Not Voted" || documentSnapshot.data().latestVotes.length !== 0) {
+                    console.log("Changing ", documentSnapshot.id, "voteStatus and latestVotes fields")
+                    return account.update({
+                        voteStatus: "Not Voted",
+                        latestVotes: []
+                    });
+                }
+            });
+        });
+    });
+
+// exports.addCreditDebit = functions.firestore
+//     .document('poll/{pollId}')
 //     .onCreate((snap, context) => {
-//       let accounts = admin.firestore.collection('accounts');
-//       accounts.get().then(querySnapshot => {
-//           querySnapshot.forEach(documentSnapshot => {
-//               console.log(`Document found at path: ${documentSnapshot.ref.path}`);
-//           });
-//       });
+//         return admin.firestore().collection('transactions').get().then(querySnapshot => {
+//             let lst = {}
+//             return querySnapshot.forEach(documentSnapshot => {
+//                 let account = admin.firestore().doc(documentSnapshot.data().accountid);
+//                 let accountid = documentSnapshot.data().accountid
+//                 if (!(documentSnapshot.data().accountid in lst)){
+//                     lst[accountid] = {credit: 0, debit: 0}
+//                 }
+//                 let price = documentSnapshot.data().price
+//                 if (documentSnapshot.data().status === "done") {
+//                     if (documentSnapshot.data().isDeposit) {
+//                         price = -1 * price
+//                         lst[accountid].credit += price
+//                     }
+//                     else {
+//                         lst[accountid].debit += price;
+//                     }
+//                 }
+//                 return account.update(lst[accountid]);
+//             });
+//         });
+//     });
+
+// exports.resetCreditDebit = functions.firestore
+//     .document('poll/{pollId}')
+//     .onCreate((snap, context) => {
+//         return admin.firestore().collection('accounts').get().then(querySnapshot => {
+//             return querySnapshot.forEach(documentSnapshot => {
+//                 let account = admin.firestore().collection('accounts').doc(documentSnapshot.id);
+//                     return account.update({
+//                         credit: 0,
+//                         debit: 0
+//                     });
+//             });
+//         });
 //     });
 
 
 exports.setupNewUser = functions.auth.user().onCreate((user) => {
-  console.log("Setting up new user: ", user);
-  let firstname = "";
-  let lastname = "";
+    console.log("Setting up new user: ", user);
+    let firstname = "";
+    let lastname = "";
 
-  firstname = user.email.split("@")[0];
+    firstname = user.email.split("@")[0];
 
-  if(user.displayName !== null) {
-    let splitName = user.displayName.split(' ');
-    if(splitName.length > 0)
-      firstname = splitName[0];
-    if(splitName.length > 1)
-      lastname = splitName[1];
-  }
-  var account = {
-    balance: 0,
-    email: user.email,
-    uid: user.uid,
-    firstname: firstname,
-    lastname: lastname,
-    lastestVotes: [],
-    voteStatus: "Not Voted"
-  };
-  return admin.firestore().collection('accounts').add(account);
+    if (user.displayName !== null) {
+        let splitName = user.displayName.split(' ');
+        if (splitName.length > 0)
+            firstname = splitName[0];
+        if (splitName.length > 1)
+            lastname = splitName[1];
+    }
+    var account = {
+        balance: 0,
+        email: user.email,
+        uid: user.uid,
+        firstname: firstname,
+        lastname: lastname,
+        latestVotes: [],
+        voteStatus: "Not Voted",
+        credit: 0,
+        debit: 0
+    };
+    return admin.firestore().collection('accounts').add(account);
 });
